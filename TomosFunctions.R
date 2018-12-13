499ce673aaa8e1f6b62785858f007660e5db2011
Tomo Eguchi <Tomo.Eguchi@noaa.gov>
2018-12-13 15:26:28 -0800

    Adapted to the new work desktop

--- TomosFunctions.R
+++ TomosFunctions.R
@@ -4,7 +4,7 @@
 # Tomo Eguchi
 # 12 February 2014 Started
 
-if (Sys.info()["nodename"] == "SWC-TEGUCHI-D")
+if (Sys.info()["nodename"] == "SWC-TEGUCHI1-D")
   .libPaths("C:/Users/tomo.eguchi/AppData/Local/R")
 
 # Extracting posterior samples of deviance or any other variable from jags output:
